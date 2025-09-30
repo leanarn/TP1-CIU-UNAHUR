af3ff177c957e62d028b1102ee4195779f5d3396
codmaez <me92monteagudo@gmail.com>
2025-09-30 17:45:09 -0300

    Carta de productos con productos estaticos.

---
+++ src/pages/Carta.jsx
@@ -0,0 +1,82 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Navbar from '../componentes/Navbar';
+import ProductCard from "../componentes/ProductCard";
+
+export default function Carta(){
+    const productos = [
+        {
+            nombre: 'Café Espresso',
+            categoria: 'bebidas',
+            descripcion: 'Un café intenso y aromático en taza pequeña.',
+            imagen: 'cafe_expresso.png',
+            precio: 800
+        },
+        {
+            nombre: 'Capuccino',
+            categoria: 'bebidas',
+            descripcion: 'Café con leche espumosa y un toque de cacao.',
+            imagen: 'capuchino.png',
+            precio: 1200
+        },
+        {
+            nombre: 'Latte',
+            categoria: 'bebidas',
+            descripcion: 'Café espresso con abundante leche vaporizada.',
+            imagen: 'latte.png',
+            precio: 1100
+        },
+        {
+            nombre: 'Té Verde',
+            categoria: 'bebidas',
+            descripcion: 'Infusión ligera, refrescante y antioxidante.',
+            imagen: 'te_verde.png',
+            precio: 700
+        },
+        {
+            nombre: 'Medialunas',
+            categoria: 'panificados',
+            descripcion: 'Clásico argentino, perfecto para acompañar el café.',
+            imagen: 'medialunas.png',
+            precio: 300
+        },
+        {
+            nombre: 'Tostado de Jamón y Queso',
+            categoria: 'panificados',
+            descripcion: 'Pan de molde con jamón y queso derretido.',
+            imagen: 'tostado_jyq.png',
+            precio: 1500
+        },
+        {
+            nombre: 'Brownie con Nuez',
+            categoria: 'pasteleria',
+            descripcion: 'Bizcocho de chocolate húmedo con nueces.',
+            imagen: 'brownie_nuez.png',
+            precio: 900
+        },
+        {
+            nombre: 'Limonada',
+            categoria: 'bebidas',
+            descripcion: 'Bebida refrescante con jugo de limón natural.',
+            imagen: 'limonada.png',
+            precio: 1000
+        }
+    ];
+
+    return(
+        <div>
+            <Navbar/>
+            <div className="container text-center">
+                <div className="row row-cols-auto" style={{ marginTop: '1rem', gap: '1rem', display: 'flex', justifyContent: 'center' }}>
+                    {
+                        productos.map(((producto, i) => (
+                            <div className="col" key={i}>
+                                <ProductCard producto={producto}/>
+                            </div>
+                        )))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
