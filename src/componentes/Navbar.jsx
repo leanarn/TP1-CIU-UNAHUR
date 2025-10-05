b0e486bf510e5acc84c56fd41e633028e3c04af9
Matias Monteagudo <me92monteagudo@gmail.com>
2025-10-05 16:10:30 -0300

    Corrección de estilos de navbar en mobile.

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,22 +1,26 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo2.png";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import styles from "./Navbar.module.css";
 
 export default function Navbar({ verCarrito }){
+    
 
     return(
         <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={logo} style={{ height: '2.5rem' }} /></a>
+                <Link to="/" className="navbar-brand d-flex" >
+                    <img className={styles.logo} src={logo} />
+                    <h1 className={`ms-2 ${styles.title}`}>Café UNAHUR</h1>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav" style={{ width: '100%' }}>
-                        <Link to="/" className="nav-link" > Inicio </Link>
-                        <Link to="/carta" className="nav-link" > Carta </Link>
-                        <Link to="/contacto" className="nav-link" > Contacto </Link>
-                        <button className="nav-link" style={{ marginLeft: 'auto' }} onClick={verCarrito}>  
+                        <Link to="/" className={`nav-link ${styles.nav_item}`} > Inicio </Link>
+                        <Link to="/carta" className={`nav-link ${styles.nav_item}`} > Carta </Link>
+                        <Link to="/contacto" className={`nav-link ${styles.nav_item}`} > Contacto </Link>
+                        <button className={`nav-link ${styles.nav_item} ${styles.nav_item_carrito}`} onClick={verCarrito}>  
                             <i className="bi bi-cart"></i> Ver carrito
                         </button>
                     </div>
