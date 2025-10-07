e2e21c73499e28a83cbfebf8980b64df3585d19a
ZoeAby <zoe.damore@estudiantes.unahur.edu.ar>
2025-10-06 23:40:14 -0300

    Se agrego la pagina de contacto con formulario

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,40 @@
+import Navbar from "../componentes/Navbar";
+
+export default function Contacto(){
+    const manejarEnvio = (e) =>{
+        e.preventDefault();
+        alert("Gracias por tu mensaje!")
+    }
+    return(
+        <div>
+            <Navbar/>
+            <div className="container"> 
+                <div className="row"> 
+                    <div className="col-sm p-4"> 
+                        <h2 style={{color:"#381f18"}}>Podes contactarnos llamando al 011-2066-1958, nos interesa tu opinion!</h2>
+                        <h3 style={{fontSize:"1.3rem", color:"#C88141"}}>Comuniquese tambien con nosotros completando el siguiente formulario:</h3>
+                    </div>
+                    <form className="col-sm p-4" onSubmit={manejarEnvio}> 
+                        <div class="form-group">
+                            <label for="formGroupExampleInput">Nombre y Apellido</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" required/>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label for="formGroupExampleInput2">Telefono</label>
+                            <input type="number" class="form-control" id="formGroupExampleInput2"/>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label for="formGroupExampleInput">E-Mail</label>
+                            <input type="email" class="form-control" id="formGroupExampleInput"/>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label for="formGroupExampleInput">Mensaje</label>
+                            <textarea type="text" class="form-control" id="formGroupExampleInput" required/>
+                        </div>
+                        <button className="btn btn-primary btn-sm mt-3" type="submit">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div> 
+    )
+}
